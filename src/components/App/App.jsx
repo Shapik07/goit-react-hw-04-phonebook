1cf234ed753b75dee2a10246da37e6778591ef4e
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-11-13 00:06:44 +0200

    Переписал файл App на Хуки

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,92 +1,61 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import ContactForm from 'components/PhoneBook/PhoneBookForm/PhoneBookForm';
 import { Section } from 'components/PhoneBook/PhoneBookSection/PhoneBookSection';
 import { ContactsList } from 'components/PhoneBook/PhoneBookContacts/PhoneBookContacts';
 import { Filter } from 'components/PhoneBook/PhoneBookFilter/PhoneBookFilter';
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export default function App() {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(window.localStorage.getItem('contacts')) ?? ''
+  );
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+    setContacts(contacts);
+  }, [contacts]);
 
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  formSubmitHandler = (name, number) => {
+  const formSubmitHandler = (name, number) => {
     const contact = {
       id: nanoid(),
       name: name,
       number: number,
     };
 
-    console.log(contact);
-
-    let contacts = this.state.contacts;
     let arrayName = contacts.map(contact => contact.name);
     arrayName.includes(contact.name)
       ? window.alert(`${contact.name} is already in contacts`)
-      : this.setState(({ contacts }) => ({
-          contacts: [contact, ...contacts],
-        }));
+      : setContacts([contact, ...contacts]);
   };
 
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
   };
 
-  handlerRemoveButton = name => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.name !== name),
-    }));
+  const handlerRemoveButton = name => {
+    setContacts(contacts => contacts.filter(contact => contact.name !== name));
 
-    this.setState({ filter: '' });
+    setFilter('');
   };
 
-  render() {
-    const { filter, contacts } = this.state;
+  const normalizedFilter = filter.toLocaleLowerCase();
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizedFilter)
+  );
 
-    const normalizedFilter = filter.toLocaleLowerCase();
-    const visibleContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-
-    return (
-      <>
-        <Section title="Phonebook">
-          <ContactForm onSubmit={this.formSubmitHandler} />
-        </Section>
-        <Section title="Contacts">
-          <Filter
-            value={this.state.filter}
-            onChange={this.changeFilter}
-          ></Filter>
-          <ContactsList
-            contacts={visibleContacts}
-            onClick={this.handlerRemoveButton}
-          />
-        </Section>
-      </>
-    );
-  }
+  return (
+    <>
+      <Section title="Phonebook">
+        <ContactForm onSubmit={formSubmitHandler} />
+      </Section>
+      <Section title="Contacts">
+        <Filter value={filter} onChange={changeFilter}></Filter>
+        <ContactsList
+          contacts={visibleContacts}
+          onClick={handlerRemoveButton}
+        />
+      </Section>
+    </>
+  );
 }
-
-export default App;
